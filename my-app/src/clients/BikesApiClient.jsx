2d703b697b2ab56bec16da65de7133963b6d964a
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-10-25 16:51:06 +0100

    add the dublin bikes layer and data

--- my-app/src/clients/BikesApiClient.jsx
+++ my-app/src/clients/BikesApiClient.jsx
@@ -1,15 +1,17 @@
 import axios from 'axios';
+import bikes from 'clients/dublin-bikes.json';
 
 const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
 };
-
 const BikesApiClient = {
+
   getBikesData: async () => {
-    const { data } = await axios.get('/dublinbikes-api/last_snapshot/', {
-      headers: HEADERS,
-    });
+    // const { data } = await axios.get('/dublinbikes-api/last_snapshot/', {
+    //   headers: HEADERS,
+    // });
+    const data = bikes;
 
     return data;
   },
--- my-app/src/components/layers/BikesLayer.js
+++ my-app/src/components/layers/BikesLayer.js
@@ -1,43 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { CartoLayer } from '@deck.gl/carto';
 import { selectSourceById } from '@carto/react-redux';
 import { useCartoLayerProps } from '@carto/react-api';
 import htmlForFeature from 'utils/htmlForFeature';
+import { MAP_TYPES } from '@deck.gl/carto';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { HexagonLayer } from '@deck.gl/aggregation-layers';
+
+import BikesApiClient from '../../clients/BikesApiClient';
 
 export const BIKES_LAYER_ID = 'bikesLayer';
-
-const colorRange = [
-  [255, 255, 178, 25],
-  [254, 217, 118, 85],
-  [254, 178, 76, 127],
-  [253, 141, 60, 170],
-  [240, 59, 32, 212],
-  [189, 0, 38, 255]
-];
-
 
 export default function BikesLayer() {
   const { bikesLayer } = useSelector((state) => state.carto.layers);
   const source = useSelector((state) => selectSourceById(state, bikesLayer?.source));
   const cartoLayerProps = useCartoLayerProps({ source });
 
+  const [bikesData, setBikesData] = useState([]);
+
+  useEffect(() => {
+    BikesApiClient.getBikesData()
+      .then(response => {
+        setBikesData(response);
+      })
+      .catch((e) => {
+        console.log('error', e);
+      });
+  }, []);
+
   if (bikesLayer && source) {
-    return new CartoLayer({
-      ...cartoLayerProps,
-      id: BIKES_LAYER_ID,
-      getFillColor: [241, 109, 122],
-      pointRadiusMinPixels: 2,
-      getLineColor: [255, 0, 0],
-      lineWidthMinPixels: 1,
-      pickable: true,
-      onHover: (info) => {
-        if (info?.object) {
-          info.object = {
-            html: htmlForFeature({ feature: info.object }),
-            style: {},
-          };
-        }
-      },
+    return new HexagonLayer({
+      id: 'hex',
+      data: bikesData,
+      getPosition: (d) => [+d.longitude, +d.latitude],
+      getElevationWeight: (d) => d.available_bike_stands + d.available_bikes,
+      elevationScale: 100,
+      extruded: true,
+      radius: 109,
+      opacity: 0.6,
+      coverage: 0.88,
+      lowerPercentile: 50,
     });
   }
 }
