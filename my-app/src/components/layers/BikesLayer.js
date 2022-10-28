50ad3d65d5dff4dfe47d8ad764f0e0622710c1bd
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-10-28 12:48:41 +0100

    add multilayer for the bikes tab

--- my-app/src/components/layers/BikesLayer.js
+++ my-app/src/components/layers/BikesLayer.js
@@ -5,8 +5,8 @@ import { selectSourceById } from '@carto/react-redux';
 import { useCartoLayerProps } from '@carto/react-api';
 import htmlForFeature from 'utils/htmlForFeature';
 import { MAP_TYPES } from '@deck.gl/carto';
-import { ScatterplotLayer } from '@deck.gl/layers';
-import { HexagonLayer } from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer, TextLayer } from '@deck.gl/layers';
+import { HexagonLayer, HeatmapLayer } from '@deck.gl/aggregation-layers';
 
 import BikesApiClient from '../../clients/BikesApiClient';
 
@@ -21,7 +21,7 @@ export default function BikesLayer() {
 
   useEffect(() => {
     BikesApiClient.getBikesData()
-      .then(response => {
+      .then((response) => {
         setBikesData(response);
       })
       .catch((e) => {
@@ -30,27 +30,55 @@ export default function BikesLayer() {
   }, []);
 
   if (bikesLayer && source) {
-    return new HexagonLayer({
-      id: 'hex',
-      data: bikesData,
-      getPosition: (bikeStation) => [+bikeStation.longitude, +bikeStation.latitude],
-      getElevationWeight: (d) => d.available_bike_stands + d.available_bikes,
-      elevationScale: 10,
-      extruded: true,
-      radius: 109,
-      opacity: 0.6,
-      coverage: 0.88,
-      lowerPercentile: 50,
-      wireframe: true,
-      pickable: true,
-      onHover: (info) => {
-        if (info?.object) {
-          info.object = {
-            html: `<div>lat: ${info.coordinate[0]}</div>`,
-            style: {},
-          };
-        }
-      },
-    });
+    return [
+      new HexagonLayer({
+        id: 'hex',
+        data: bikesData,
+        getPosition: (bikeStation) => [+bikeStation.longitude, +bikeStation.latitude],
+        getElevationWeight: (d) => d.available_bike_stands + d.available_bikes,
+        elevationScale: 10,
+        extruded: true,
+        radius: 109,
+        opacity: 0.6,
+        coverage: 0.88,
+        lowerPercentile: 50,
+        wireframe: true,
+        pickable: true,
+        onHover: (info) => {
+          if (info?.object) {
+            info.object = {
+              html: `<div>lat: ${info.coordinate[0]}</div>`,
+              style: {},
+            };
+          }
+        },
+      }),
+      new HeatmapLayer({
+        id: 'heatmapLayer',
+        data: bikesData,
+        // getPosition: (d) => d.COORDINATES,
+        getPosition: (bikeStation) => [+bikeStation.longitude, +bikeStation.latitude],
+        // getWeight: (d) => d.WEIGHT,
+        getWeight: (bikeStation) => bikeStation.weight,
+        // getWeight: 10,
+        aggregation: 'SUM',
+      }),
+      new TextLayer({
+        id: 'text-layer',
+        data: bikesData,
+        pickable: true,
+        // getPosition: d => d.coordinates,
+        getPosition: (bikeStation) => [+bikeStation.longitude, +bikeStation.latitude],
+        // getText: d => d.name,
+        getText: (bikeStation) => bikeStation.name,
+        getSize: 12,
+        getAngle: 0,
+        getTextAnchor: 'middle',
+        getAlignmentBaseline: 'center',
+        background: false,
+        // getColor: (bikeStation) => [105, 205, 255, 200],
+        getColor: [105, 205, 255, 200],
+      })
+    ];
   }
 }
